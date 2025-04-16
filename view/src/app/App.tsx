d9adf871a298e285ff74f15e18a8c78d31131c88
“selam” <Selamkd@users.noreply.github.com>
2025-04-16 13:00:26 +0100

    migrate to vite, downgrade tailwind version, tailwind config, vite config, add main entry html

--- view/src/app/App.tsx
+++ view/src/app/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>APP</p>
-      </header>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Tailwind CSS Test</h1>
+
+      <div className="border border-black mb-6">
+        <div className="App border bg-pink-500 border-black p-4 mb-4">
+          <header className="App-header bg-pink-400 text-3xl p-2">
+            <p className="text-4xl">APP (Original)</p>
+          </header>
+        </div>
+      </div>
+
+      <div className="border border-black mb-6">
+        <div className="App border border-black p-4 mb-4 bg-red-500">
+          <header className="App-header p-2 bg-red-400 text-3xl">
+            <p className="text-4xl">APP (Red Test)</p>
+          </header>
+        </div>
+      </div>
+
+      <div className="border border-black mb-6">
+        <div className="App border border-black p-4 mb-4 bg-primary-500">
+          <header className="App-header p-2 bg-primary-400 text-3xl">
+            <p className="text-4xl">APP (Primary Test)</p>
+          </header>
+        </div>
+      </div>
     </div>
   );
 }
