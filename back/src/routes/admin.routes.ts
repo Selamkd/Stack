e1e290ce6de4f12317f8c3df0bc3cc333d03d682
“selam” <Selamkd@users.noreply.github.com>
2025-04-19 15:27:28 +0100

    simple admin auth with access key, add limiter to pass requests, add package express-rate-limit

---
+++ back/src/routes/admin.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import * as AdminController from '../controllers/admin.controller';
+
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: 'Too many attempts. Please try again later.',
+});
+
+const router = Router();
+
+router.post('/check-pass', limiter, AdminController.checkPass);
+
+export default router;
--- back/src/routes/app.routes.ts
+++ back/src/routes/app.routes.ts
@@ -1,16 +1,22 @@
 import { Router } from 'express';
 import tagRoutes from './tag.routes';
+
 import noteRoutes from './note.routes';
 import snippetRoutes from './snippet.routes';
 import quickLookupRoutes from './lookups.routes';
+import adminRoutes from './admin.routes';
 import logger from '../utils/logger';
 
 const router = Router();
+
 router.use(logger.middleware());
+
 router.use('/tags', tagRoutes);
 router.use('/notes', noteRoutes);
 router.use('/snippets', snippetRoutes);
+router.use('/check-pass', adminRoutes);
 router.use('/quicklookups', quickLookupRoutes);
+
 router.get('/ping', (req, res) => {
   res.status(200).json({ status: 'ok', timestamp: new Date().toISOString() });
 });
