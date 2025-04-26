5c7ac6ba26c8ed65c8a78e26bd085fa4c8e5d01a
“selam” <Selamkd@users.noreply.github.com>
2025-04-26 22:16:23 +0100

    snippets list

--- view/src/app/Snippets.tsx
+++ view/src/app/Snippets.tsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from 'react';
+import { ISnippet } from '../../../back/src/models/snippet.model';
+import APIService from '../service/api.service';
+import { format } from 'date-fns';
+
 export default function Snippets() {
-  return <h1>Snippets List</h1>;
+  const [snippets, setSnippets] = useState<ISnippet[]>();
+
+  useEffect(() => {
+    async function getSnippets() {
+      try {
+        const snippetsRes = await APIService.get('snippets');
+
+        setSnippets(snippetsRes);
+      } catch (e) {
+        console.error('Error fetching snippets', e);
+      }
+    }
+    getSnippets();
+  }, []);
+
+  return (
+    <main className="bg-custom-surface max-w-7xl mx-auto h-screen p-5 m-5 rounded-lg">
+      {snippets && (
+        <div className="flex flex-col border border-custom-border h-full p-3 gap-y-5">
+          {snippets.map((snippet) => (
+            <div className="flex justify-between" key={snippet._id}>
+              <p className="font-semibold text-1xl">{snippet.title}</p>
+              <p className="font-semibold text-1xl">{snippet.description}</p>
+              <span>
+                {snippet.createdAt
+                  ? format(new Date(snippet.createdAt), 'PP')
+                  : 'No date'}
+              </span>
+
+              <p>{snippet.category?.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
