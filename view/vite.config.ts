0b1c2fce98af7d359afdb3c8ed2efd3a35182cbf
“selam” <Selamkd@users.noreply.github.com>
2025-07-20 14:46:21 +0100

    [back]: update deploy script

--- view/vite.config.ts
+++ view/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import dts from 'vite-plugin-dts';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
