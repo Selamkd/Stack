17f3ec03de57fec1e33b39f2149f6b039eac390e
“selam” <Selamkd@users.noreply.github.com>
2025-07-26 21:01:55 +0100

    install dnd-kit , add ticket droppable container

--- view/src/app/ProjectBoard.tsx
+++ view/src/app/ProjectBoard.tsx
@@ -1,9 +1,11 @@
 import { Search, Trash2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import APIService from '../service/api.service';
+import { DndContext, useDraggable } from '@dnd-kit/core';
+import TicketDroppable from '../components/TicketDroppable';
 
 //placeholder ticket interface till model setup
-interface ITicket {
+export interface ITicket {
   _id: string;
   title: string;
   description: string;
@@ -15,6 +17,12 @@ export default function ProjectBoard() {
   const [search, setSearch] = useState('');
   const [deleteMessage, setDeleteMessage] = useState<string | null>(null);
   const STAGES = ['Parked', 'Backlog', 'In Development', 'Done'];
+
+  function Draggable() {
+    const { attributes, listeners, setNodeRef } = useDraggable({
+      id: 'unique-id',
+    });
+  }
   async function getTickets() {
     try {
       //   const ticketRes = await APIService.get('ticket');
@@ -50,66 +58,68 @@ export default function ProjectBoard() {
   });
 
   return (
-    <main className="max-w-9xl mx-auto min-h-screen p-4 md:p-6 my-2 md:my-6 rounded-lg">
-      <div className="flex flex-col mb-4">
-        <h1 className="text-3xl">Project Tracker Board</h1>
-      </div>
-
-      <div className="flex flex-col md:flex-row gap-4 mb-8">
-        <div className="flex-1 relative">
-          <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4 text-[#404040]" />
-          <input
-            type="text"
-            placeholder="Search tickets..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="w-full pl-11 pr-4 py-3 bg-[#141414] border border-[#242424] rounded-lg text-white placeholder-[#404040] focus:outline-none focus:border-[#303030]"
-          />
+    <DndContext>
+      <main className="max-w-9xl mx-auto min-h-screen p-4 md:p-6 my-2 md:my-6 rounded-lg">
+        <div className="flex flex-col mb-4">
+          <h1 className="text-3xl">Project Tracker Board</h1>
         </div>
-      </div>
 
-      {deleteMessage && (
-        <div className="flex items-center gap-6 mb-5 ml-2">
-          {deleteMessage && (
-            <span
-              onClick={() => setDeleteMessage(null)}
-              className="text-xs text-red-200/80 cursor-pointer"
-            >
-              {deleteMessage}
-            </span>
-          )}
+        <div className="flex flex-col md:flex-row gap-4 mb-8">
+          <div className="flex-1 relative">
+            <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4 text-[#404040]" />
+            <input
+              type="text"
+              placeholder="Search tickets..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-full pl-11 pr-4 py-3 bg-[#141414] border border-[#242424] rounded-lg text-white placeholder-[#404040] focus:outline-none focus:border-[#303030]"
+            />
+          </div>
         </div>
-      )}
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-        {' '}
-        {STAGES.map((stage) => (
-          <div className="flex flex-col">
-            <header className="px-4 py-3 border border-custom-border bg-custom-hover/80 rounded-t-lg">
-              <p className="text-center font-medium">{stage}</p>
-            </header>
 
-            <div className="flex-1 border-l border-r border-b border-custom-border/80 rounded-b-lg bg-custom-hover/20 p-2 min-h-[400px]">
-              <div className="space-y-3">
-                {filtered.map((ticket) => (
-                  <TicketCard
-                    key={ticket._id}
-                    ticket={ticket}
-                    handleDeleteTicket={() => handleDeleteTicket(ticket._id)}
-                    refresh={getTickets}
-                  />
-                ))}
+        {deleteMessage && (
+          <div className="flex items-center gap-6 mb-5 ml-2">
+            {deleteMessage && (
+              <span
+                onClick={() => setDeleteMessage(null)}
+                className="text-xs text-red-200/80 cursor-pointer"
+              >
+                {deleteMessage}
+              </span>
+            )}
+          </div>
+        )}
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+          {' '}
+          {STAGES.map((stage) => (
+            <div className="flex flex-col">
+              <header className="px-4 py-3 border border-custom-border bg-custom-hover/80 rounded-t-lg">
+                <p className="text-center font-medium">{stage}</p>
+              </header>
+
+              <div className="flex-1 border-l border-r border-b border-custom-border/80 rounded-b-lg bg-custom-hover/20 p-2 min-h-[400px]">
+                <div className="space-y-3">
+                  {filtered.map((ticket) => (
+                    <TicketCard
+                      key={ticket._id}
+                      ticket={ticket}
+                      handleDeleteTicket={() => handleDeleteTicket(ticket._id)}
+                      refresh={getTickets}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </main>
+          ))}
+        </div>
+      </main>
+    </DndContext>
   );
 }
 interface ITicketCard {
   ticket: ITicket;
   handleDeleteTicket: (ticketId: string) => void;
-  refresh: () => void;
+  refresh?: () => void;
 }
 
 export function TicketCard(props: ITicketCard) {
